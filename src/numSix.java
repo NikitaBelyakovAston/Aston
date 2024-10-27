37effa5136746d111e879e0c1e67c150ff05860e
Nikita <lancreve@gmail.com>
2024-10-27 16:19:45 +0300

    Delete src directory

--- src/numSix.java
+++
@@ -1,9 +0,0 @@
-public class numSix {
-    public static void main (String[] args){
-        checkNumPosNeg ();
-    }
-    public static void checkNumPosNeg (){
-        int a = 0;
-        System.out.println(a>=0 ? "Положительное" : "Отрицательное");
-    }
-}
